f6120757b8f3b0ee7ba525796109cb45848d0a57
Bodecker DellaMaria <bojdell@gmail.com>
2015-05-03 12:22:06 -0500

    fixed deadlocking

--- mutex.py
+++ mutex.py
@@ -10,13 +10,14 @@ import math
 message_queues = {}
 N = 9
 DEBUG_MODE = False
+option = 0
 
 class Node():
 	"""
 	Node Class: contains functionality for a node in the Maekawa-esque network
 	"""
 
-	def __init__(self, nodeID, next_req, cs_int):
+	def __init__(self, nodeID, cs_int, next_req):
 		self.nodeID = nodeID
 		self.next_req = int(next_req)
 		self.cs_int = int(cs_int)
@@ -24,10 +25,15 @@ class Node():
 		self.voting_set = set()
 		for i in range(1, self.vote_size + 1):
 			self.voting_set.add((nodeID + i)%N)
-		self.replies_received = {}
 		self.queued_reqs = Queue.PriorityQueue()
 		self.has_replied = False
-		self.in_cs = False
+		self.grant_req = None
+		self.reset_params()
+
+		# create thread to listen for incoming messages
+		listenThread = threading.Thread(target=self.__listen)
+		listenThread.setDaemon(True)
+		listenThread.start()
 
 	def __str__(self):
 		result = "Node: " + str(self.nodeID) + "\n"
@@ -36,12 +42,7 @@ class Node():
 	def init(self):
 		# wait for other nodes to get set up
 		while len(message_queues) < N:
-			pass
-
-		# create thread to listen for incoming messages
-		listenThread = threading.Thread(target=self.__listen)
-		listenThread.setDaemon(True)
-		listenThread.start()
+			time.sleep(0.01)
 
 		# create thread to process state machine
 		smThread = threading.Thread(target=self.__init)
@@ -86,112 +87,131 @@ class Node():
 		req_msg = Message("request", time.time(), self.nodeID)
 		for nodeID in self.voting_set:
 			message_queues[nodeID].put((req_msg.ts, req_msg))
-			if DEBUG_MODE:
-				print str(self.nodeID) + " request -> " + str(nodeID)
 
 		# wait to receive all votes, TODO: handle deadlock
-		while len(self.replies_received) < self.vote_size:
-			time.sleep(0.01)
+		while len(self.grants_received) < self.vote_size:
+			time.sleep(0.01)		
 
 		self.in_cs = True
+
+		# print for logging
+		result = "{0:.6f} {1}".format(time.time(), self.nodeID)
+		for voter in self.voting_set:
+			result += " " + str(voter)
+		print result
+
+		if DEBUG_MODE:
+			print str(self.nodeID) + " has entered CS "
 
 	def __exit_cs(self):
 		# send out release messages to members of voting set
 		release_msg = Message("release", time.time(), self.nodeID)
 		for nodeID in self.voting_set:
 			message_queues[nodeID].put((release_msg.ts, release_msg))
-			if DEBUG_MODE:
-				print str(self.nodeID) + " release -> " + str(nodeID)
 
 		self.reset_params()
+		if DEBUG_MODE:
+			print str(self.nodeID) + " has exited CS "
 
 	def reset_params(self):
-		self.replies_received = {}
+		self.grants_received = {}
 		self.in_cs = False
 		self.recv_failed = False
-		self.yield_no_grant = False
-		self.grant_priority = None
+		self.yield_no_grant = set()
 
 	def process_message(self, message):
-		if message.msg is "request":
+		if message.msg == "request":
 			# if we've already replied before the next release, queue requests
 			if self.has_replied:
-				self.queued_reqs.put((message.src_nodeID, message))
-				if self.grant_priority and self.grant_priority > message.src_nodeID:
+				self.queued_reqs.put((message.ts, message))
+				if DEBUG_MODE:
+					print str(self.nodeID) + ": " + str(self.grant_req) + ", " + str(message.src_nodeID)
+				if self.grant_req.ts > message.ts:
 					msg = Message("failed", time.time(), self.nodeID)
 					message_queues[message.src_nodeID].put((msg.ts, msg))
-					if DEBUG_MODE:
-						print str(self.nodeID) + " failed -> " + str(message.src_nodeID)
 				else:
 					msg = Message("inquire", time.time(), self.nodeID)
-					message_queues[self.grant_priority].put((msg.ts, msg))
+					message_queues[self.grant_req.src_nodeID].put((msg.ts, msg))
 
 			# else, grant this request
 			else:
 				grant_msg = Message("grant", time.time(), self.nodeID)
 				message_queues[message.src_nodeID].put((grant_msg.ts, grant_msg))
-				self.grant_priority = message.src_nodeID
-				if DEBUG_MODE:
-					print str(self.nodeID) + " grant -> " + str(message.src_nodeID)
-				
+
 				# indicate we have replied before receiving a release msg
 				self.has_replied = True
+				self.grant_req = message
 
-		elif message.msg is "grant":
+		elif message.msg == "grant":
 			# record grant
-			replies_received[message.src_nodeID] = message
-			self.yield_no_grant = False
+			self.grants_received[message.src_nodeID] = message
+			if message.src_nodeID in self.yield_no_grant:
+				self.yield_no_grant.remove(message.src_nodeID)
 
-		elif message.msg is "release":
+		elif message.msg == "release":
 			# if we have no queued requests, reset has_replied flag
 			if self.queued_reqs.empty():
 				self.has_replied = False
+				self.grant_req = None
 
 			# else, handle next queued req
 			else:
 				next_req = self.queued_reqs.get()[1]
 				grant_msg = Message("grant", time.time(), self.nodeID)
 				message_queues[next_req.src_nodeID].put((grant_msg.ts, grant_msg))
-				if DEBUG_MODE:
-					print str(self.nodeID) + " grant -> " + str(next_req.src_nodeID)
 
 				# indicate we have replied
 				self.has_replied = True
+				self.grant_req = next_req
 
-		elif message.msg is "inquire":
-			del replies_received[message.src_nodeID]
-			time.sleep(0.05)
-			if self.in_cs:
-				replies_received[message.src_nodeID] = message
-			else:
-				if self.recv_failed or self.yield_no_grant:
-					grant_msg = Message("yield", time.time(), self.nodeID)
-					message_queues[message.src_nodeID].put(grant_msg.ts, grant_msg)
-					self.yield_no_grant = True
-				else:
-					replies_received[message.src_nodeID] = message
+		elif message.msg == "inquire":
+			if not self.in_cs and (self.recv_failed or len(self.yield_no_grant) > 0):
+				grant_msg = Message("yield", time.time(), self.nodeID)
+				message_queues[message.src_nodeID].put((grant_msg.ts, grant_msg))
+				self.yield_no_grant.add(message.src_nodeID)
+				del self.grants_received[message.src_nodeID]
+			# del self.grants_received[message.src_nodeID]
+			# time.sleep(0.05)
+			# if self.in_cs:
+			# 	self.grants_received[message.src_nodeID] = message
+			# else:
+			# 	if self.recv_failed or self.yield_no_grant:
+			# 		grant_msg = Message("yield", time.time(), self.nodeID)
+			# 		message_queues[message.src_nodeID].put(grant_msg.ts, grant_msg)
+			# 		self.yield_no_grant = True
+			# 	else:
+			# 		self.grants_received[message.src_nodeID] = message
 			
-		elif message.msg is "yield":
+		elif message.msg == "yield":
 			next_req = self.queued_reqs.get()[1]
 			grant_msg = Message("grant", time.time(), self.nodeID)
-			self.grant_priority = next_req.src_nodeID
 			message_queues[next_req.src_nodeID].put((grant_msg.ts, grant_msg))
 
 			message.msg = "request"
-			self.queued_reqs.put((message.src_nodeID, message))
+			self.queued_reqs.put((message.ts, message))
 
-		elif message.msg is "failed":
+			# indicate we have replied
+			self.has_replied = True
+			self.grant_req = next_req
+
+		elif message.msg == "failed":
 			self.recv_failed = True
 
 	def __listen(self):
+		global option
 		while True:
 			time.sleep(0.01)
 
 			# if we've received data, process it
 			if not message_queues[self.nodeID].empty():
 				msg = message_queues[self.nodeID].get()[1]
-				if DEBUG_MODE:
-					print "msg received at node " + str(self.nodeID) + ": " + str(msg)
+				
+				# print for logging
+				if option:
+					result = "{0:.6f} {1} {2} {3}".format(time.time(), self.nodeID, msg.src_nodeID, msg.msg)
+					print result
+
+				# process the message
 				self.process_message(msg)
 
 class Message():
@@ -206,7 +226,7 @@ class Message():
 
 	def __str__(self):
 		result = "msg: " + str(self.msg) + ", "
-		result += "ts: " + str(self.ts) + ", "
+		result += "ts: " + "{0:.6f}".format(self.ts) + ", "
 		result += "src_nodeID: " + str(self.src_nodeID)
 		return result
 
@@ -216,10 +236,12 @@ if __name__ == "__main__":
 		print "Usage: python mutex.py <cs_int> <next_req> <tot_exec_time> <option>"
 		sys.exit()
 
+	global option
+
 	cs_int = sys.argv[1]
 	next_req = sys.argv[2]
 	tot_exec_time = sys.argv[3]
-	option = sys.argv[4]
+	option = int(sys.argv[4])
 
 	nodes = []
 	
@@ -233,6 +255,9 @@ if __name__ == "__main__":
 	if DEBUG_MODE:
 		raw_input("press enter...")
 
+	# nodes[0].init()
+	# nodes[1].init()
+
 	for i in range(0, N):
 		nodes[i].init()
 
@@ -240,5 +265,6 @@ if __name__ == "__main__":
 	while time.time() - start < float(tot_exec_time):
 		time.sleep(0.01)
 
-	print "=== Total Execution Time Reached ==="
+	if DEBUG_MODE:
+		print "=== Total Execution Time Reached ==="
 
